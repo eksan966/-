d7a345d001700fd91f977c8471c77c90fcd34661
Zer0neK <chuxia031212@gmail.com>
2025-10-09 19:45:17 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -15,35 +15,14 @@
     ╚═════╝   ╚═════╝  ╚══════╝ ╚══════╝    ╚═╝   
 ]]
     
-function executeBasedOnDetection(executionCode)
 
-    local cleanCode = string.gsub(executionCode, "%s+", " ")
+if getfenv().ADittoKey then
+
+    local Key = getfenv().ADittoKey
     
-    local isDirectExecution = cleanCode == 'loadstring(game:HttpGet("https://jianke.lol/init.lua"))()'
+    getfenv().ADittoKey = Key
+    loadstring(request({Url = "https://github.com/Zer0neK/Sword_Guest/releases/download/SG/Loader.lua"}).Body)()
+else
     
-    local keyValue = string.match(cleanCode, 'getfenv%(%)%.ADittoKey%s*=%s*"([^"]*)"')
-    local hasInitCall = string.find(cleanCode, 'loadstring%(%s*game:HttpGet%("https://jianke.lol/init.lua"%)%s*%)%s*%(%)')
-    
-    if isDirectExecution then
-
-        print("执行免费版本...")
-        loadstring(request({Url = "https://github.com/Zer0neK/Sword_Guest/releases/download/SG/FREE.lua"}).Body)()
-        
-    elseif keyValue and hasInitCall then
-
-        print("执行授权版本，卡密: " .. keyValue)
-        
-
-        local Key = keyValue
-
-        getfenv().ADittoKey = Key
-        
-        loadstring(request({Url = "https://github.com/Zer0neK/Sword_Guest/releases/download/SG/Loader.lua"}).Body)()
-        
-    else
-
-    end
+    loadstring(request({Url = "https://github.com/Zer0neK/Sword_Guest/releases/download/SG/FREE.lua"}).Body)()
 end
-
-
-
